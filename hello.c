0507534c0139570c12b278d159f634724757671a
jschager3 <jschager3@huskers.unl.edu>
2022-08-25 15:40:45 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jacob
+ *         jschager3@huskers.unl.edu
+ * Date:   2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n Jacob!!\n Computer Science\n");
+
+  return 0;
+}
